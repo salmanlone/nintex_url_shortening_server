4a70e6d09987c633c1d9ad7db8ac16bf0a31f246
Salman Tariq Lone <salmanlone89@gmail.com>
2019-06-26 16:33:52 +0800

    cors

--- src/app.ts
+++ src/app.ts
@@ -3,24 +3,28 @@ const models = require('./db/models/urlShortenSchema');
 import Routes from "./config/routes";
 import bodyParser = require('body-parser');
 var restifySwaggerJsdoc = require('restify-swagger-jsdoc');
+import * as corsMiddleware from "restify-cors-middleware";
+
+const cors = corsMiddleware({
+    origins: ["*"],
+    allowHeaders: ["Authorization"],
+    exposeHeaders: ["Authorization"]
+});
 
 export default class App {
 
     private server: any;
+
     constructor() {
         this.server = restify.createServer();
         this.server.use(bodyParser.json());
+        this.server.pre(cors.preflight);
+        this.server.use(cors.actual);
         Routes.apply(this.server);
         this.server.use(
             bodyParser.urlencoded({
                 extended: true
             }));
-        // restifySwaggerJsdoc.createSwaggerPage({
-        //     title: 'API documentation', // Page title
-        //     version: '1.0.0', // Server version
-        //     server: this.server, // Restify server instance created with restify.createServer()
-        //     path: '/docs/swagger' // Public url where the swagger page will be available
-        // });
     }
 
     public GetServer() {
